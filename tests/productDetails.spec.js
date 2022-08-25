0d04d769de99f87ca9db95ac7df013a485b61382
Tarcisio R Sacramento <tarcisacra333@gmail.com>
2022-08-24 21:20:00 -0400

    Refatorando requisito 6, depois de melhor entendimento refatorei com base na Documentação do jestjs.io

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -34,22 +34,36 @@ describe('6 - Implemente os casos de teste para a função `productDetails`', ()
     expect(typeof productDetails).toEqual('function');
   })
   it('Teste se o retorno da função é um array', () => {
-    expect([].productDetails === 'Array').toBe(false)
+    expect(Array.isArray(productDetails())).toBeTruthy()
   })
   it('Teste se o array retornado pela função contem dois itens dentro', () => {
     expect(Object.keys(productDetails()).length).toEqual(2)
   })
   it('Teste se os dois itens dentro do array retornado pela função sao objeto', () => {
-    expect(typeof productDetails()).toBe('object')
+    expect(typeof productDetails()).toEqual('object');
+    expect(typeof productDetails()[0] === typeof productDetails()[1])
+    //acredito q o seguinte da falso positivo
+    // expect(typeof productDetails()).toBe('object')
   })
   it('Teste se os dois parametros igual passados retornam objetos iguais', () => {
-    expect(productDetails('Desinfetante', 'Sabonete')).not.toBe()
+    expect(productDetails()[0] !== productDetails()[1]).toBeTruthy() //esse sim saiu da minha cabeça HEHEHE
+    // O teste seguinte "entendi" por pressao se é me entende.
+    // expect(productDetails('Desinfetante', 'Sabonete')).not.toBe()
   })
-  it('Teste se os dois productIds terminam com 123', () => {
-    expect(productDetails('Desinfetante', 'Sabonete')[0].details.productId).toBe('Desinfetante123');
-    expect(productDetails('Desinfetante', 'Sabonete')[1].details.productId).toEqual('Sabonete123')
+  it('Teste se os dois productIds terminam com 123', () => { 
+    expect(productDetails()[0].details.productId).toMatch(/123/)
+    expect(productDetails()[0].details.productId).toContain('123')
+    expect(productDetails()[1].details.productId).toMatch(/123/)
+    expect(productDetails()[1].details.productId).toContain('123')
   })
 });
 
 // Segundo test refencia a seguir.
 // https://pt.stackoverflow.com/questions/77190/como-saber-se-um-objeto-%C3%A9-um-array-em-javascript-sem-jquery#:~:text=O%20m%C3%A9todo%20Array.,e%20false%20se%20n%C3%A3o%20%C3%A9.
+// Tinha lido errado
+// Agora Sim aehuaehuah
+
+// Referencia do ultimo test
+// https://jestjs.io/pt-BR/docs/expect#tomatchregexp--string
+
+// Outros testes feitos com base na documentação do https://jestjs.io/pt-BR/docs/expect 
